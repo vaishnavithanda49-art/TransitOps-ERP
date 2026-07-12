d9148c3bc52ef807b87ee35c6c60eebe92299938
vaishnavithanda49-art <vaishnavithanda49@gmail.com>
2026-07-12 12:13:17 +0530

    Initial React frontend setup with Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-blue-100 flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-blue-700">
+        TransitOps ERP 🚛
+      </h1>
+    </div>
+  );
+}
+
+export default App;
